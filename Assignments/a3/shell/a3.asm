c760a17b6ff8e841e0469d5e65810a76b172d168
Colin Gagich <gagichce@mcmaster.ca>
2015-03-01 17:52:57 -0500

    rand function is now working

--- Assignments/a3/shell/a3.asm
+++ Assignments/a3/shell/a3.asm
@@ -6,7 +6,6 @@ segment .bss
 
 stringlabel resb 100
 
-
 segment .text
 	global asm_main
 
@@ -21,89 +20,14 @@ asm_main:
 	leave
 	ret
 
-reverse:
-
+;assuming eax is address of start of num array, ebx is length of array
+shellsort:
 	enter 0,0
 	pusha
-
-	mov eax, stringlabel
-
-	call readstring
-
-	call strlen
-
-	sub eax, 1
-
-	imul eax, 4	
-
-	add eax, stringlabel	
-
-	mov ebx, stringlabel
-
-loopRev:
-
-	;reverse the chars
-	mov ecx, [eax]
-	mov edx, [ebx]
-	mov [ebx], ecx
-	mov [eax], edx
 	
-	dump_regs 1
-	;move pointers to new characters
-	add dword ebx, 4
-	sub dword eax, 4
-	;check to see if more swapping is required
-	cmp eax, ebx
-	jg loopRev
+	push eax
+	push ebx	
 
 	popa
-	leave
-	ret
-
-strlen:
-	enter 0,0
-	push ebx
-
-	mov ebx, eax
-	mov eax, 0
-
-	;iterate until null character is found
-loops:
-	cmp dword [ebx] , 0
-	
-	je short done
-
-	sub ebx, 4
-	add eax, 1
-	jmp loops
-
-done:
-	pop ebx
-	leave
-	ret
-
-readstring:
-	enter 0,0
-	push ebx	
-	push eax
-	mov ebx, eax
-
-	;continually read a character until the null character is entered
-readSection:
-	call read_char
-
-	mov [ebx], eax	
-
-	sub ebx, 4
-	cmp eax, 0
-
-	jne readSection
-	
-	;read the enter character the user pressed. not sure if this will cause problems as 
-	call read_char
-	
-	;restore eax
-	pop eax
-	pop ebx
-	leave
+	leave 
 	ret
--- Assignments/a3/shell/driver.c
+++ Assignments/a3/shell/driver.c
@@ -1,6 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void genRandArray(int * A, int n);
+int asm_main();
+
 int main()
 {
 	int ret_status;
 	ret_status = asm_main();
+	
+	for(int i = 1; i < 11; i++){
+		int *ptr;
+		int length = i*10000000;
+		ptr = (int*)malloc(length*sizeof(int));
+		if(ptr==NULL){
+			printf("Memory could not be allocated! You must download more rams!"); 
+			return 0;
+		}
+		genRandArray(ptr, length);
+		
+		printf("%d\n", *(ptr));
+		free(ptr);
+	}	
+
 	return ret_status;
+}
+
+//this assumes the memory has already been allocated, just needs to be filled
+void genRandArray(int * A, int n)
+{
+	printf("making array of rands %d long\n", n);
+	for (int i = 0; i < n; i++){
+		*(A + i) = rand();
+	}
+	//flushin' because buffers
+	fflush(stdout);
 }
